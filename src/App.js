f38774fed1da101182da02a37e63ebf31167af78
Muhammad Usama <liontiger49@yahoo.com>
2019-10-18 02:17:51 +0500

    completed signin and signup

--- src/App.js
+++ src/App.js
@@ -28,9 +28,9 @@ class App extends React.Component {
               id:snapShot.id,
               ...snapShot.data()
             }
-          })
-          console.log(this.state);
-        })
+          });
+
+        });
         
       }else{
         this.setState({currentUser:userAuth}); 
